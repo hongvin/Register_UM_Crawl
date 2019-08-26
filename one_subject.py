34bd4a9787ce8959d7cd029e3e4631f51a84f1ea
Koay Hong Vin <32212455+khvmaths@users.noreply.github.com>
2019-08-26 14:45:29 +0800

    Create one_subject.py

---
+++ one_subject.py
@@ -0,0 +1,52 @@
+##BY hongvin
+##github.com/khvmaths
+COURSE_CODE = 'GIT1004'
+REFRESH_TIME = 3
+
+from bs4 import BeautifulSoup
+import pandas as pd
+import pyttsx3
+import requests
+import time
+import datetime
+import threading
+
+site = "http://register.um.edu.my/jadual_penawaranbi.asp?s_Sesi=2019%2F2020&s_Semester=1&s_fakulti=0&s_SLOT_KOD_SUBJEK="+COURSE_CODE
+engine = pyttsx3.init()
+
+def startEngine():
+    t=threading.Timer(REFRESH_TIME,startEngine).start()
+
+    print('['+str(datetime.datetime.now().time())+']: List refreshed!')
+    response = requests.get(site)
+    soup = BeautifulSoup(response.text,"html.parser")
+    tr = soup.find_all('tr')
+    td= tr[8].find_all('td')
+
+    for br in soup.find_all('br'):
+        br.replace_with(' ')
+
+    pp=[]
+    for i in td[4:7]:
+        try:
+            pp.append(i.text)
+        except:
+            print('Error lol')
+
+    group=pp[0].split()
+    capacity=pp[1].split()
+    vacancy=pp[2].split()
+
+    df=pd.DataFrame({'Capacity':capacity,'Vacancy':vacancy})
+    df.index=group
+    df['Capacity']=df['Capacity'].astype(int)
+    df['Vacancy']=df['Vacancy'].astype(int)
+    print(df)
+
+    for i in range(len(group)):
+        if not (df.iat[i,0] == 0):
+            engine.say('Vacancy found Group'+str(i+1))
+            print('['+str(datetime.datetime.now().time())+']: FOUND!: GROUP '+str(i))
+            engine.runAndWait() 
+
+startEngine()
